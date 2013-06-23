04bb4462bc1a3a4034f3af08e6861d9148460b03
Ward W. Vuillemot <wwv@mac.com>
2013-06-22 17:11:53 -0700

    OmniAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,15 +20,15 @@ class User < ActiveRecord::Base
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     unless user
-      user = User.create(name:auth.extra.raw_info.name,
-                           provider:auth.provider,
-                           uid:auth.uid,
-                           email:auth.info.email,
-                           name:auth.info.name,
-                           username:auth.info.username,
-                           image_url:auth.info.image,
-                           password:Devise.friendly_token[0,20]
-                           )
+      user = User.create(
+         provider:auth.provider,
+         uid:auth.uid,
+         email:auth.info.email,
+         name:auth.info.name,
+         username:auth.info.username,
+         image_url:auth.info.image,
+         password:Devise.friendly_token[0,20]
+      )
     end
     user
   end
