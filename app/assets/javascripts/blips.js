f9ebfa6add899ebc64cfd6ec21dd417dabcf2c1a
Ward W. Vuillemot <wwv@mac.com>
2013-06-25 00:06:14 -0700

    Score

--- app/assets/javascripts/blips.js
+++ app/assets/javascripts/blips.js
@@ -1,5 +1,6 @@
 var BlipLight = (function (blipManager) {
   this.blipManager      = blipManager;
+  this.isClicked        = true;
   
   this.animate = function(domNode, blipPositionLeft, blipPositionTop, blipDuration, blipDistance, blipWidth, blipLetter){    
     blipNode = jQuery('<div/>', {
@@ -7,8 +8,37 @@ var BlipLight = (function (blipManager) {
         style: 'left: ' + blipPositionLeft + 'px; top: ' + blipPositionTop + 'px; height: ' + blipWidth + 'px; width: ' + blipWidth + 'px;' + 'line-height: ' + blipWidth + 'px;',
         text: blipLetter });
 
-    domNode.append(blipNode);
     self = this;
+
+    if (blipLetter.length == 1)
+    {
+      var mouseLeave = null;
+      blipNode.mouseenter(function(){
+        if(mouseLeave){
+          clearTimeout( mouseLeave );          
+        }
+        $(this).addClass('hovered');
+        $(this).stop();
+      });
+      blipNode.mouseleave(function(){
+        if(!this.isClicked){
+          blip = this;
+          mouseLeave = setTimeout(function () { 
+            $(blip).removeClass('hovered');
+            $(blip).fadeOut(500).remove();
+          }, 500);          
+        }      
+      });
+      blipNode.click(function(){
+        this.isClicked = true;
+        $(this).removeClass('hovered');
+        $(this).addClass('selected');
+        $(this).stop();
+        self.blipManager.answer($(this).html());
+      });
+    }
+    
+    domNode.append(blipNode);
     // animate( end-conditions, duration in milliseconds, end-state call-back )
     blipNode.animate(
       {
@@ -30,22 +60,69 @@ var BlipLight = (function (blipManager) {
 var BlipManager = ( function(maxNumberOfBlips, domNodeParent) {
   var _maxNumberOfBlips         = maxNumberOfBlips;
   var _domNodeParent            = $(domNodeParent);
+  var _questionsAnswered        = 0;
+  var _score                    = 0;
+  var _total                    = 0;
+  var _right                    = 0;
+  var _wrong                    = 0;
+  var _blipManager              = this;
+  
+  var _pairs = [
+      {  question: 'foo',
+         answer: 'bar'
+      },
+      {  question: 'marco',
+         answer: 'polo'
+      },
+    ];
+    
+  _total = _pairs.length;
+  
+  $('#score').html(_score); 
+  $('#right').html(_right); 
+  $('#wrong').html(_wrong); 
+  $('#total').html(_total); 
+
+  $('#erase').click(function(){
+    $('#answer').html('');
+    $('#status').html('&nbsp;');
+    $('.selected').fadeOut(500).remove();
+  });
+  
+  $('#next').click(function(){
+    _blipManager.nextQuestion();
+    _wrong++;
+    _blipManager._score(_pointsPerWordWrong);
+    $('#wrong').html(_wrong);
+    $('#answer').html('');
+    $('#status').html('&nbsp;');
+    $('.selected').fadeOut(500).hide();
+  });
+  
+  
+  $('#question').html(_pairs[_questionsAnswered].question + '?');
   
   // initial state
   var _numberOfBlips            = 0;
   var _running                  = false;
   
   // constraints for the blip of light  
-  var _minimumLetterDuration    = 3000;
-  var _minimumLetterWidth       = 30;
+  var _minimumLetterDuration    = 5000;
+  var _minimumLetterWidth       = 40;
   var _minimumBlipWidth         = 1;
-  var _maximumBlipWidth         = 40;
+  var _maximumBlipWidth         = 60;
   var _minimumBlipDistance      = 100;
   var _maximumBlipDuration      = 10000;
   var _footerHeight             = 60;
-  var _AInAscii                 = 65;
+  var _AInAscii                 = 97;
   var _emptyBlipCharacter       = '';
   var _lettersInAlphabet        = 26;
+  
+  var _pointsPerLetterRight      = 20;
+  var _pointsPerLetterWrong      = -10;
+  var _pointsPerWordRight        = 40;
+  var _pointsPerWordWrong        = -30;
+  var _pointPerClear             = -20;
   
   // done - called by blip of light when its journey is done
   this.done = function(){
@@ -57,6 +134,7 @@ var BlipManager = ( function(maxNumberOfBlips, domNodeParent) {
   this.pause = function(){
     this._setPauseButton();
     this._setRunning(false);
+    this._setControls();
   }
   
   // resume - user calls to resume blips
@@ -75,6 +153,7 @@ var BlipManager = ( function(maxNumberOfBlips, domNodeParent) {
   this.init = function(){
     this._getRunning();
     this._setRunning(_running);
+    this._setControls();
     if(_running){
       this._setResumeButton();
       for(var blip = _numberOfBlips; blip < _maxNumberOfBlips; blip++)
@@ -84,6 +163,56 @@ var BlipManager = ( function(maxNumberOfBlips, domNodeParent) {
     } else {
       this._setPauseButton();
     }
+  }
+  
+  this.nextQuestion = function(){
+    _questionsAnswered++;
+    if(_questionsAnswered < _pairs.length){
+      $('#status').show().html('&nbsp');
+      $('.selected').fadeOut(500).remove();
+      $('#answer').show().html('&nbsp;');
+      $('#question').html(_pairs[_questionsAnswered].question + '?');      
+    }
+    else
+    {
+      _questionsAnswered = 0;
+      $('#answer').fadeOut(500);
+      $('#next').fadeOut(500);
+      $('#erase').fadeOut(500);
+      $('#status').fadeIn(1000).html("there aint no more.");
+      $('#question').hide().fadeIn(5000).html('fin.');
+      this.pause();
+    }
+  }  
+  
+  this.answer = function(letter){
+    $('#answer').append(letter);
+    answer = _pairs[_questionsAnswered].answer;
+    player_answer = $('#answer').html();
+    regex = RegExp(player_answer + '.*','i');
+    if(player_answer == answer){
+      $('#status').show().html('yatzhee.');
+      this._score(_pointsPerWordRight);
+      _right++;
+      $('#right').html(_right);
+      this.nextQuestion();
+    } else if(answer.match(regex)){
+      this._score(_pointsPerLetterRight);
+    } else {
+      this._score(_pointsPerLetterWrong);
+      $('#status').show().html('huh?');
+      $('#status').delay(1000).fadeOut(1000).delay(10).show();      
+    }
+  };
+  
+  this._right = function(){
+    _right++;
+    $('#right').html(_right);
+  }
+  this._score = function(incrementScore)
+  {
+    _score += incrementScore
+    $('#score').html(_score);
   }
 
   // private methods
@@ -118,6 +247,15 @@ var BlipManager = ( function(maxNumberOfBlips, domNodeParent) {
       blipLight.animate(_domNodeParent, blipPositionLeft, blipPositionTop, blipDuration, blipDistance, blipWidth, blipLetter);
     }      
   };  
+  
+  this._setControls = function(){
+    if(_running){
+      $('#controls').show();
+    }
+    else{
+      $('#controls').hide();
+    }
+  }
   
   this._setPauseButton = function(){
     $('#blipManager').html("<i class='icon-play'></i>");
