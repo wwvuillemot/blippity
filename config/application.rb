13b32f73f3241e0b1e37c2d041d2c9eab8fc7f7d
Ward W. Vuillemot <wwv@mac.com>
2013-06-25 11:03:21 -0700

    Heroku

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,8 @@ module Blippity
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # https://devcenter.heroku.com/articles/rails-asset-pipeline
+    config.assets.initialize_on_precompile = false
   end
 end
