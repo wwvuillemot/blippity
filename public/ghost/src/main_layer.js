dc165bb9eefc88660c68927578430985f0433a13
Ward W. Vuillemot <wwv@mac.com>
2013-07-07 17:40:58 -0700

    Ghost
    - projectiles

--- public/ghost/src/main_layer.js
+++ public/ghost/src/main_layer.js
@@ -1,6 +1,6 @@
 // 1
 var MainLayer = cc.LayerColor.extend({
- 
+    _projectiles:[],
     _monsters:[],
     // 2
     ctor:function() {
@@ -13,6 +13,13 @@ var MainLayer = cc.LayerColor.extend({
     // 4
     onEnter:function () {
         this._super();
+        
+        if( 'touches' in sys.capabilities ) {
+            this.setTouchEnabled(true);
+        }
+        if( 'mouse' in sys.capabilities ) {
+            this.setMouseEnabled(true);
+        }
  
         // 5
         var player = cc.Sprite.create(res_player);
@@ -24,6 +31,7 @@ var MainLayer = cc.LayerColor.extend({
         this.addChild(player);
         this.schedule(this.gameLogic, 3);
     },
+    
     addMonster:function() {
  
         var monster = cc.Sprite.create(res_monster);
@@ -58,9 +66,62 @@ var MainLayer = cc.LayerColor.extend({
         this._monsters.push(monster); // 6
  
     },
+    
     gameLogic:function(dt) {
         this.addMonster();
-    }
+    },
+    locationTapped:function(location) {
+        // Set up initial location of the projectile
+        var projectile = cc.Sprite.create(res_projectile);
+        projectile.setPosition(20, winSize.height/2);
+ 
+        // Determine offset of location to projectile
+        var offset = cc.pSub(location, projectile.getPosition()); // 1
+ 
+        // Bail out if you are shooting down or backwards
+        if (offset.x <= 0) return;
+ 
+        // Ok to add now - we've double checked position
+        this.addChild(projectile);
+ 
+        // Figure out final destination of projectile
+        var realX = winSize.width + (projectile.getContentSize().width / 2);
+        var ratio = offset.y / offset.x;
+        var realY = (realX * ratio) + projectile.getPosition().y;
+        var realDest = cc.p(realX, realY);
+ 
+        // Determine the length of how far you're shooting
+        var offset = cc.pSub(realDest, projectile.getPosition());
+        var length = cc.pLength(offset);
+        var velocity = 480.0;
+        var realMoveDuration = length / velocity;
+ 
+        // Move projectile to actual endpoint
+        projectile.runAction(cc.Sequence.create( // 2
+            cc.MoveTo.create(realMoveDuration, realDest),
+            cc.CallFunc.create(function(node) {
+                cc.ArrayRemoveObject(this._projectiles, node);
+                node.removeFromParent();
+            }, this)
+        ));
+ 
+        // Add to array
+        projectile.setTag(2);
+        this._projectiles.push(projectile);
+    },
+ 
+    onMouseUp:function (event) {
+        var location = event.getLocation();
+        this.locationTapped(location);
+    },
+ 
+    onTouchesEnded:function (touches, event) {
+        if (touches.length <= 0)
+            return;
+        var touch = touches[0];
+        var location = touch.getLocation();
+        this.locationTapped(location);
+    }    
  
 });
 
