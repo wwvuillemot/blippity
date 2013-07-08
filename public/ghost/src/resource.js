a84426e9f4e4e9a16a66e65c5e79b083ca9be0e6
Ward W. Vuillemot <wwv@mac.com>
2013-07-07 17:35:47 -0700

    Ghost
    - add moving ghost

--- public/ghost/src/resource.js
+++ public/ghost/src/resource.js
@@ -1,7 +1,13 @@
 var resource_directory = "/ghost/res/";
 var sounds_directory   = "/ghost/snd/";
-var res_player = resource_directory + "player.png";
+var res_player         = resource_directory + "player.png";
+var res_monster        = resource_directory + "monster.png";
+var res_projectile     = resource_directory + "projectile.png";
  
 var g_resources = [
-    {type:"image", src:res_player}
+ 
+    {type:"image", src:res_player},
+    {type:"image", src:res_monster},
+    {type:"image", src:res_projectile}
+ 
 ];
